b0f95f37ff9131a6d6192bd0f3059a1863d8f1b5
BabiPouetPouet <youri.dhont@hotmail.com>
2020-08-11 17:11:15 +0200

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit b45d887089ce8cf7fb5ba5623a6503dc763bee75, reversing
    changes made to bc0e6336aa5a8bc4e2c2366dc778d7951715e51d.

--- Scripts/Procedural Animation/spiderMovement.cs
+++ Scripts/Procedural Animation/spiderMovement.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(SpiderDetectionsEvent))]
 public class spiderMovement : MonoBehaviour
 {
     public List<TargetStep> leftTargetSteps;
@@ -11,52 +8,22 @@ public class spiderMovement : MonoBehaviour
     public float stepShift = 2f;
     public float speed;
     public AnimationCurve breathCurve;
+    public float distanceToChangeDistance = 3f;
     public float distanceToGround = 2f;
     float breathTimer;
-    Vector2 _dir = Vector2.left;
+    Vector2 dir = Vector2.left;
     float heightPosition;
     [SerializeField] bool inversed;
-    [SerializeField] bool shiftToFacePlayer;
-    SpiderDetectionsEvent detectionEvent;
 
     private void Start()
     {
-        detectionEvent = GetComponent<SpiderDetectionsEvent>();
-        detectionEvent.OnWallIsNextBy += OnWallIsNextHandler;
-        detectionEvent.OnPlayerDetected += OnPlayerDetectedHandler;        
-        heightPosition = transform.position.y;
         if (inversed)
-            _dir = Vector2.right;
-
-        detectionEvent.ShiftDirection(new SpiderDetectionsEvent.ShiftDirectionEventArgs { newDir = _dir });
+            dir = Vector2.right;
     }
-
-    private void OnPlayerDetectedHandler(object sender, SpiderDetectionsEvent.PlayerDetectedEventArgs e)
-    {
-        if (shiftToFacePlayer)
-        {
-            if (e.player.transform.position.x < transform.position.x && _dir == Vector2.right)
-            {
-                ShiftDirection(_dir);
-                detectionEvent.ShiftDirection(new SpiderDetectionsEvent.ShiftDirectionEventArgs { newDir = _dir });
-            }
-            if (e.player.transform.position.x > transform.position.x && _dir == Vector2.left)
-            {
-                ShiftDirection(_dir);
-                detectionEvent.ShiftDirection(new SpiderDetectionsEvent.ShiftDirectionEventArgs { newDir = _dir });
-            }
-        }        
-    }
-
-    private void OnWallIsNextHandler(object sender, SpiderDetectionsEvent.WallIsNextByEventArgs e)
-    {
-        ShiftDirection(_dir);
-        detectionEvent.ShiftDirection(new SpiderDetectionsEvent.ShiftDirectionEventArgs { newDir = _dir});        
-    }
-
     void Update()
     {
         CheckHeightPosition();
+        CheckWall();
         Breath();
         Move();
     }
@@ -89,41 +56,61 @@ public class spiderMovement : MonoBehaviour
     private void Breath()
     {
         breathTimer += Time.deltaTime;
-        if (breathTimer > breathCurve.keys[breathCurve.length -1].time)
+        if (breathTimer > breathCurve.length)
         {
             breathTimer = 0;
         }
         transform.position = new Vector3(transform.position.x, heightPosition + breathCurve.Evaluate(breathTimer));
     }
-    private void ShiftDirection(Vector2 dir)
+    private void CheckWall()
     {
-        if (dir == Vector2.left)
+        RaycastHit2D hit = Physics2D.Raycast(new
+        Vector2(transform.position.x, transform.position.y),
+        dir, distanceToChangeDistance, LayerMask.GetMask("Ground"));
+        if (hit)
         {
-            foreach (TargetStep target in rightTargetSteps)
+            if (dir == Vector2.left)
             {
-                target.transform.position = new Vector3(target.transform.position.x + stepShift, target.transform.position.y, target.transform.position.y);
+                foreach (TargetStep target in rightTargetSteps)
+                {
+                    target.transform.position = new Vector3(target.transform.position.x + stepShift, target.transform.position.y, target.transform.position.y);
+                }
+                foreach (TargetStep target in leftTargetSteps)
+                {
+                    target.transform.position = new Vector3(target.transform.position.x + stepShift, target.transform.position.y, target.transform.position.y);
+                }
+                dir = Vector2.right;
             }
-            foreach (TargetStep target in leftTargetSteps)
+            else
             {
-                target.transform.position = new Vector3(target.transform.position.x + stepShift, target.transform.position.y, target.transform.position.y);
+                foreach (TargetStep target in rightTargetSteps)
+                {
+                    target.transform.position = new Vector3(target.transform.position.x - stepShift, target.transform.position.y, target.transform.position.y);
+                }
+                foreach (TargetStep target in leftTargetSteps)
+                {
+                    target.transform.position = new Vector3(target.transform.position.x - stepShift, target.transform.position.y, target.transform.position.y);
+                }
+                dir = Vector2.left;
             }
-            _dir = Vector2.right;
-        }
-        else
-        {
-            foreach (TargetStep target in rightTargetSteps)
-            {
-                target.transform.position = new Vector3(target.transform.position.x - stepShift, target.transform.position.y, target.transform.position.y);
-            }
-            foreach (TargetStep target in leftTargetSteps)
-            {
-                target.transform.position = new Vector3(target.transform.position.x - stepShift, target.transform.position.y, target.transform.position.y);
-            }
-            _dir = Vector2.left;
         }
     }
     private void Move()
     {
-            transform.Translate(_dir * speed * Time.deltaTime);
+        if (dir == Vector2.left)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, dir * distanceToChangeDistance);
     }
 }
--- Scripts/WeaponController.cs
+++ Scripts/WeaponController.cs
@@ -27,15 +27,6 @@ public class WeaponController : MonoBehaviour
     float _spreadAngle;
     bool _canShoot = false;
 
-    public float _reloadTime;
-    public int _magazineCapacity;
-    public int _maxCapacity;
-    public Light2D _magazineLight;
-    public float _maxMagazineIntensity;
-    public Light2D _emptyMagazineLight;
-
-
-
     void Start()
     {
         _camera = Camera.main;
@@ -53,28 +44,23 @@ public class WeaponController : MonoBehaviour
         if (_orientation.x < 0)
         {
             GameState._isCharacterFlipped = true;
-            _parent.transform.localScale = new Vector3(-1, 1, 1);
-            //_parent.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
-            if (_orientation.y < transform.localPosition.y)
-                transform.eulerAngles = new Vector3(0, 0, (Mathf.Clamp(tempAngle, -180,-180 + _lightAngleRange/2))-180);
-                //transform.eulerAngles = new Vector3(0, 0, Mathf.Clamp(tempAngle, -180, -180 + _lightAngleRange/2));
+            _parent.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            if(_orientation.y < transform.localPosition.y)
+                transform.eulerAngles = new Vector3(0, 0, Mathf.Clamp(tempAngle, -180, -180 + _lightAngleRange/2));
             else
-                transform.eulerAngles = new Vector3(0, 0, Mathf.Clamp(tempAngle, 180 - _lightAngleRange / 2, 180)-180);
+                transform.eulerAngles = new Vector3(0, 0, Mathf.Clamp(tempAngle, 180 - _lightAngleRange / 2, 180));
         }
         else
         {
 
             GameState._isCharacterFlipped = false;
-
-            _parent.transform.localScale = new Vector3(1, 1, 1);
-            //_parent.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            _parent.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
             transform.eulerAngles = new Vector3(0, 0, Mathf.Clamp(tempAngle, - _lightAngleRange / 2, _lightAngleRange / 2));
         }
 
         if (Input.GetKey(KeyCode.Mouse1))
         {
-            if(_magazineCapacity>0)
-                _canShoot = true;
+            _canShoot = true;
             _pointLight.pointLightInnerAngle = Mathf.Clamp(_pointLight.pointLightInnerAngle -=_toAdd, _minAngle, _maxAngle);
             _pointLight.pointLightOuterAngle = Mathf.Clamp(_pointLight.pointLightOuterAngle -=_toAdd, _minAngle + 10, _maxAngle);
             _spreadAngle = _pointLight.pointLightInnerAngle;
@@ -84,7 +70,7 @@ public class WeaponController : MonoBehaviour
             _pointLight.pointLightInnerAngle = Mathf.Clamp(_pointLight.pointLightInnerAngle += _toAdd/3f, _minAngle, _maxAngle);
             _pointLight.pointLightOuterAngle = Mathf.Clamp(_pointLight.pointLightOuterAngle += _toAdd/3f, _minAngle + 10, _maxAngle);
             _spreadAngle = _pointLight.pointLightInnerAngle;
-            if(_pointLight.pointLightInnerAngle == _maxAngle || _magazineCapacity <= 0)
+            if(_pointLight.pointLightInnerAngle == _maxAngle)
             {
                 _canShoot = false;
             }
@@ -92,16 +78,10 @@ public class WeaponController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
         {
             StartCoroutine(ShootWithSpread(_spreadAngle, _pelletNumber, transform.rotation, _spawner.position));
-            _magazineCapacity--;
-            if (_magazineCapacity <= 0)
-            {
-                StartCoroutine(Reloading());
-            }
             _canShoot = false;
             _pointLight.pointLightOuterAngle = _maxAngle;
             _pointLight.pointLightOuterAngle = _maxAngle;
         }
-        SetMagazineLight();
     }
     private void OnValidate()
     {
@@ -109,34 +89,18 @@ public class WeaponController : MonoBehaviour
             _minAngle = _maxAngle;
     }
 
-    void SetMagazineLight()
-    {
-        _magazineLight.intensity = (_magazineCapacity/ (float)_maxCapacity)*_maxMagazineIntensity;
-        _emptyMagazineLight.intensity = _maxMagazineIntensity - _magazineLight.intensity;
-    }
 
     IEnumerator ShootWithSpread(float spreadAngle, int numberOfPellets, Quaternion transformRotation, Vector3 spawnerPosition)
     {
         for(int i = 0; i < numberOfPellets; i++)
         {
             GameObject pellet = Instantiate(_projectile, spawnerPosition, transformRotation);
-            if(GameState._isCharacterFlipped)
-                pellet.transform.rotation = Quaternion.Euler(new Vector3(0, 0, transformRotation.eulerAngles.z + Random.Range(-spreadAngle, spreadAngle)+180));
-            else
-                pellet.transform.rotation = Quaternion.Euler(new Vector3(0, 0, transformRotation.eulerAngles.z + Random.Range(-spreadAngle, spreadAngle)));
+            pellet.transform.rotation = Quaternion.Euler(new Vector3(0, 0, transformRotation.eulerAngles.z + Random.Range(-spreadAngle, spreadAngle)));
             pellet.GetComponent<Rigidbody2D>().AddForce(pellet.transform.right * _firePower);
             Destroy(pellet, 2f);
             yield return new WaitForEndOfFrame();
             yield return new WaitForSeconds(.01f);
 
         }
-    }
-
-    IEnumerator Reloading()
-    {
-        _canShoot = false;
-        yield return new WaitForSeconds(_reloadTime);
-        _canShoot = true;
-        _magazineCapacity = _maxCapacity;
     }
 }
